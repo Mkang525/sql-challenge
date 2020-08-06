e64ec1663dfde54dfa0631089d3b2bac8678c355
MonicaKang <mokaex@gmail.com>
2020-08-06 16:59:41 -0700

    update

--- EmployeeSQL/ERD_homework.sql
+++ EmployeeSQL/ERD_homework.sql
@@ -1,8 +1,3 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/Mllzbx
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
 CREATE TABLE "departments" (
     "dept_no" VARCHAR   NOT NULL,
     "dept_name" VARCHAR   NOT NULL,
@@ -66,4 +61,41 @@ REFERENCES "titles" ("title_id");
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
+SELECT * FROM departments
+SELECT * FROM dept_emp
+SELECT * FROM dept_manager
 SELECT * FROM employees
+SELECT * FROM salaries
+SELECT * FROM titles
+
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+SELECT 
+	employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salaries
+FROM employees
+INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '%1986'
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+
+
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
