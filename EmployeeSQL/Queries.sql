caec8105fc9132370a46c9bc68347ff6a116e1e9
MonicaKang <mokaex@gmail.com>
2020-08-07 22:10:00 -0700

    update

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -1,73 +1,3 @@
-CREATE TABLE "departments" (
-    "dept_no" VARCHAR   NOT NULL,
-    "dept_name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
-);
-
-CREATE TABLE "dept_emp" (
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL
-
-);
-
-CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR   NOT NULL,
-    "emp_no" INT   NOT NULL
-);
-
-CREATE TABLE "employees" (
-    "emp_no" INT   NOT NULL,
-    "emp_title" VARCHAR   NOT NULL,
-    "birth_date" VARCHAR   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "sex" VARCHAR   NOT NULL,
-    "hire_date" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-CREATE TABLE "salaries" (
-    "emp_no" INT   NOT NULL,
-    "salaries" INT   NOT NULL
-);
-
-CREATE TABLE "titles" (
-    "title_id" VARCHAR   NOT NULL,
-    "staff" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
-);
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
-REFERENCES "titles" ("title_id");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-SELECT * FROM departments
-SELECT * FROM dept_emp
-SELECT * FROM dept_manager
-SELECT * FROM employees
-SELECT * FROM salaries
-SELECT * FROM titles
-
 --List the following details of each employee: employee number, last name, first name, sex, and salary.
 
 SELECT 
@@ -175,6 +105,7 @@ GROUP BY last_name
 ORDER BY last_name_count DESC
 
 --BONUS: Average salary by title
+--Merged data from employees table and salaries table, created view 
 
 SELECT e.emp_no, e.emp_title, s.salaries
 FROM employees as e
@@ -201,4 +132,3 @@ SELECT t.staff, nt.emp_title, nt.avg_salary
 FROM name_title AS nt
 JOIN titles as t
 ON (nt.emp_title=t.title_id)
-
